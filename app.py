c81ed7cc7151beb9174c9e54682a73d7359dc14f
matejsarlija <matejsarlija1@gmail.com>
2020-12-01 08:20:09 +0100

    flask still bugging me

--- app.py
+++ app.py
@@ -4,21 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ \
                 os.path.join(basedir, 'db.sqlite3')
 
-
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 
 with app.app_context():
     db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,111 @@
+from app import app
+from models import User, Note, UserSchema, NoteSchema
+from flask import jsonify, request
+
+note_schema = NoteSchema()
+notes_schema = NoteSchema(many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+# hello world
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# all users
+@app.route("/user", methods=["GET"])
+def get_user():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result)
+
+
+# endpoint to create new user
+@app.route("/user", methods=["POST"])
+def add_user(id):
+    username = request.json['username']
+    email = request.json['email']
+
+    new_user = User(username, email)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user)
+
+# get user detail by id
+@app.route("/user/<id>", methods=["GET"])
+def user_detail(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+
+@app.route("/user/<id>", methods=["PUT"])
+def user_update(id):
+    user = User.query.get(id)
+    username = request.json['username']
+    email = request.json['email']
+
+    user.email = email
+    user.username = username
+
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
+# ditto for notes
+@app.route('/note/')
+def note_list():
+    all_notes = Note.query.all()
+    return jsonify(notes_schema.dump(all_notes))
+
+
+@app.route('/note/', methods=['POST'])
+def create_note():
+    body = request.json.get('body', '')
+
+    note = Note(body=body)
+
+    db.session.add(note)
+    db.session.commit()
+
+    return note_schema.jsonify(note)
+
+
+@app.route('/note/<int:note_id>/', methods=["GET"])
+def note_detail(note_id):
+    note = Note.query.get(note_id)
+    return note_schema.jsonify(note)
+
+
+@app.route('/note/<int:note_id>/', methods=['PATCH'])
+def update_note(note_id):
+    body = request.json.get('body', '')
+
+    note = Note.query.get(note_id)
+
+    note.body = body
+
+    db.session.add(note)
+    db.session.commit()
+
+    return note_schema.jsonify(note)
+
+
+@app.route('/note/<int:note_id>/', methods=["DELETE"])
+def delete_note(note_id):
+    note = Note.query.get(note_id)
+
+    db.session.delete(note)
+    db.session.commit()
+
+    return note_schema.jsonify(note)
